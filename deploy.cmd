dfddcad6ed835b619d2ec124d5203b69ce0e8755
nate.wernimont@gmail.com <nate.wernimont@gmail.com>
2019-04-14 19:47:49 -0500

    Use original deployment and checkin dist

--- deploy.cmd
+++ deploy.cmd
@@ -89,10 +89,10 @@ goto :EOF
 echo Handling node.js deployment.
 
 :: 1. KuduSync
-::IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-::  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-::  IF !ERRORLEVEL! NEQ 0 goto error
-::)
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
 
 :: 2. Select node version
 call :SelectNodeVersion
@@ -104,11 +104,6 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
-
-:: call :ExecuteCmd !NPM_CMD! install -g @angular/cli
-call :ExecuteCmd !NPM_CMD! update
-call :ExecuteCmd ng update
-call :ExecuteCmd ng build natewdev --main "%DEPLOYMENT_SOURCE%" --output-path "%DEPLOYMENT_TARGET%" --delete-output-path
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
